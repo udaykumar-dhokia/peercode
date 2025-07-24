42a9cfa928c024e7eb0e37c281b51a6c02a16823
udthedeveloper <udaykumardhokia@gmail.com>
2025-07-24 16:29:50 +0530

    React Redux Setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -22,7 +24,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StrictMode>
   );
 }
